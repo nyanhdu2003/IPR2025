6f9c56af135ef4cc0d6fd831b4bc3cb7b462df1b
NQ Huy <nyanhdu2003@gmail.com>
2025-04-10 16:22:55 +0700

    3. Create example controller class

---
+++ QRPackingApp.WebAPI/Controllers/ClassController1.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QRPackingApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassController1 : ControllerBase
+    {
+    }
+}
